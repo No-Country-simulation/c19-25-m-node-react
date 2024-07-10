a34ac4c05c45ce292dcbc363e9027ec9cf1dc470
Miguel Toyas Pernichi <168015510+mitoperni@users.noreply.github.com>
2024-07-10 19:33:12 +0000

    Nuevo Home Realizado

--- FRONTEND-REACT/src/pages/Home.jsx
+++ FRONTEND-REACT/src/pages/Home.jsx
@@ -1,72 +1,33 @@
 import React, { useState, useEffect } from 'react';
-import CardHome from '../components/CardHome.jsx'
 
 
 
 
 export default function Home() {
-  const [reservations, setReservations] = useState([]);
-  const [showReservations, setShowReservations] = useState(true);
-
-
-  useEffect(() => {
-    // Aquí deberías hacer una llamada a la API para obtener las reservas
-    // A continuación, un ejemplo de datos simulados:
-    const fetchedReservations = [
-      {
-        id: 1,
-        profileImage: 'https://via.placeholder.com/150',
-        name: 'Perico de los Palotes',
-        date: '2024-07-15',
-        rolUsuario: 'Paseador',
-      },
-      {
-        id: 2,
-        profileImage: 'https://via.placeholder.com/150',
-        name: 'Gerónimo Stilton',
-        date: '2024-08-01',
-        rolUsuario: 'Cuidador a tiempo completo',
-      },
-    ];
-    setReservations(fetchedReservations);
-  }, []);
-
-  const today = new Date().toISOString().split('T')[0];
-
-  const toggleReservations = () => {
-    setShowReservations(prevState => !prevState);
-  };
-
 
   return (
     <>
       <div className="text-start w-auto mx-3 p-0 mt-5">
-        <div className="">
-          <h1>Bienvenido <strong>USUARIO</strong> a tu página principal:</h1>
+        <div className='container-lg'>
+          <div className="d-flex justify-content-between mx-5 align-items-center">
+            <img src="https://via.placeholder.com/250" alt="Imagen de usuario" />
+            <h1>Bienvenido a Yo te cuido <strong>USUARIO</strong></h1>
+          </div>
+          <div className='d-flex flex-column justify-content-center align-items-center'>
+            <h5>Un poco sobre nosotros:</h5>
+            <h3>En Yo te Cuido podrás encontrar encontrar a personas que se preocupen por tu mascota</h3>
+            <hr className='w-50'/>
+            <h5>Ponemos en contacto a personas que necesitan cuidado para sus mascotas con cuidadores especializados.</h5>
+          </div>
         </div>
 
-        <div className="btn-group btn-gruop-lg mt-5 mx-auto mx-sm-5 col-12 col-sm-8 col-md-6 col-lg-4 col-xl-3" role="group">
-          <button type="button" className={`btn ${showReservations ? 'btn-secondary' : 'btn-outline-secondary'}`}
-            onClick={toggleReservations}> Próximas reservas</button>
-          <button type="button" className="btn btn-outline-secondary"> Ver más</button>
-        </div>
-        
-        {showReservations && (
-          <div className="container mt-4">
-            <h2>Próximas reservas:</h2>
-            <div className="row mt-3">
-              {reservations.filter(reservation => reservation.date > today).map((reservation) => (
-                <CardHome
-                  key={reservation.id}
-                  profileImage={reservation.profileImage}
-                  name={reservation.name}
-                  date={reservation.date}
-                  rolUsuario={reservation.rolUsuario}
-                />
-              ))}
-            </div>
+        <div className='container'>
+          <div className='row'>
+            
           </div>
-        )}
+        </div>
+
+        
       </div>
 
     </>
