7aa7bb36eb94b36db87a9b712f80ff791da71212
Miguel Toyas Pernichi <168015510+mitoperni@users.noreply.github.com>
2024-07-10 09:00:41 +0000

    Home Preliminar terminado

--- FRONTEND-REACT/src/App.jsx
+++ FRONTEND-REACT/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import {Route,Routes,Link } from 'react-router-dom'
+import { Route, Routes, Link } from 'react-router-dom'
 import Home from './routes/Home.jsx'
 import Login from './routes/Login.jsx'
 import Perfil from './routes/Perfil.jsx'
@@ -14,15 +14,19 @@ function App() {
 
   return (
     <>
-      <Header/>
-      <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/login' element={<Login/>}/>
-          <Route path='/perfil' element={<Perfil/>}/>
-          <Route path='/registrar' element={<Registrar/>}/>
-          <Route path='/dashboard' element={<Dashboard/>}/>
-          <Route path='/anuncios' element={<Anuncios/>}/>
-      </Routes>
+      <div className='marginBottomHeader'>
+        <Header />
+      </div>
+      <div>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/perfil' element={<Perfil />} />
+          <Route path='/registrar' element={<Registrar />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/anuncios' element={<Anuncios />} />
+        </Routes>
+      </div>
     </>
   )
 }
--- FRONTEND-REACT/src/pages/Home.jsx
+++ FRONTEND-REACT/src/pages/Home.jsx
@@ -6,6 +6,8 @@ import CardHome from '../components/CardHome.jsx'
 
 export default function Home() {
   const [reservations, setReservations] = useState([]);
+  const [showReservations, setShowReservations] = useState(true);
+
 
   useEffect(() => {
     // Aquí deberías hacer una llamada a la API para obtener las reservas
@@ -14,14 +16,16 @@ export default function Home() {
       {
         id: 1,
         profileImage: 'https://via.placeholder.com/150',
-        name: 'John Doe',
+        name: 'Perico de los Palotes',
         date: '2024-07-15',
+        rolUsuario: 'Paseador',
       },
       {
         id: 2,
         profileImage: 'https://via.placeholder.com/150',
-        name: 'Jane Smith',
+        name: 'Gerónimo Stilton',
         date: '2024-08-01',
+        rolUsuario: 'Cuidador a tiempo completo',
       },
     ];
     setReservations(fetchedReservations);
@@ -29,32 +33,40 @@ export default function Home() {
 
   const today = new Date().toISOString().split('T')[0];
 
+  const toggleReservations = () => {
+    setShowReservations(prevState => !prevState);
+  };
+
 
   return (
     <>
-      <div className="text-start w-auto mx-5 p-0">
+      <div className="text-start w-auto mx-3 p-0 mt-5">
         <div className="">
           <h1>Bienvenido <strong>USUARIO</strong> a tu página principal:</h1>
         </div>
 
-        <div className="btn-group btn-gruop-lg w-50 mt-5" role="group">
-          <button type="button" className="btn btn-outline-secondary"> Próximas reservas</button>
+        <div className="btn-group btn-gruop-lg mt-5 mx-auto mx-sm-5 col-12 col-sm-8 col-md-6 col-lg-4 col-xl-3" role="group">
+          <button type="button" className={`btn ${showReservations ? 'btn-secondary' : 'btn-outline-secondary'}`}
+            onClick={toggleReservations}> Próximas reservas</button>
           <button type="button" className="btn btn-outline-secondary"> Ver más</button>
         </div>
 
-        <div className="container mt-5">
-          <h2>Próximas reservas</h2>
-          <div className="row mt-5">
-            {reservations.filter(reservation => reservation.date > today).map((reservation) => (
-              <CardHome
-                key={reservation.id}
-                profileImage={reservation.profileImage}
-                name={reservation.name}
-                date={reservation.date}
-              />
-            ))}
+        {showReservations && (
+          <div className="container mt-4">
+            <h2>Próximas reservas:</h2>
+            <div className="row mt-3">
+              {reservations.filter(reservation => reservation.date > today).map((reservation) => (
+                <CardHome
+                  key={reservation.id}
+                  profileImage={reservation.profileImage}
+                  name={reservation.name}
+                  date={reservation.date}
+                  rolUsuario={reservation.rolUsuario}
+                />
+              ))}
+            </div>
           </div>
-        </div>
+        )}
       </div>
 
     </>
