4f190338d87588073e5a22758144f7aafb43887b
Miguel Toyas Pernichi <168015510+mitoperni@users.noreply.github.com>
2024-07-14 14:33:00 +0000

    Navbar estilizada

--- FRONTEND-REACT/src/components/Header/Navbar.jsx
+++ FRONTEND-REACT/src/components/Header/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   const location = useLocation();
 
   return (
-    <nav className="navbar navbar-expand-lg py-1" id="navbarPrincipal" >
+    <nav className="navbar navbar-expand-lg py-1" id="navbarPrincipal">
       <div className="container-fluid d-flex justify-content-between">
         <button
           className="navbar-toggler custom-toggler"
@@ -20,64 +20,64 @@ export default function Navbar() {
           <span className="navbar-toggler-icon"></span>
         </button>
         <span className="fs-3 ms-2"><strong>Yo lo cuido</strong></span>
-        <div className="collapse navbar-collapse text-lg-center ms-2 mt-2 p-0 row" id="navbarNav">
+        <div className="collapse navbar-collapse text-lg-center ms-2 p-0 row" id="navbarNav">
           <ul className="navbar-nav d-flex justify-content-end">
-            <li className="nav-item">
+            <li className="nav-item mx-2">
               <Link
-                className={`nav-link ${location.pathname === "/" ? "active" : ""}`}
+                className={`nav-link py-3 ${location.pathname === "/" ? "active" : ""}`}
                 aria-current="page"
                 to="/"
               >
                 <i className="fa-solid fa-house"></i>
               </Link>
             </li>
-            <li className="nav-item">
+            <li className="nav-item mx-2">
               <Link
-                className={`nav-link ${location.pathname === "/anuncios" ? "active" : ""}`}
+                className={`nav-link py-3 ${location.pathname === "/anuncios" ? "active" : ""}`}
                 to="/anuncios"
               >
                 Crear Anuncio
               </Link>
             </li>
-            <li className="nav-item">
+            <li className="nav-item mx-2">
               <Link
-                className={`nav-link ${location.pathname === "/perfil" ? "active" : ""}`}
+                className={`nav-link py-3 ${location.pathname === "/perfil" ? "active" : ""}`}
                 to="/perfil"
               >
                 Perfil
               </Link>
             </li>
-            <li className="nav-item">
+            <li className="nav-item mx-2">
               <Link
-                className={`nav-link ${location.pathname === "/buscador" ? "active" : ""}`}
+                className={`nav-link py-3 ${location.pathname === "/buscador" ? "active" : ""}`}
                 to="/buscador"
               >
                 Buscador
               </Link>
             </li>
-            <li className="nav-item">
+            <li className="nav-item mx-2">
               <Link
-                className={`nav-link ${location.pathname === "/login" ? "active" : ""} bg-white rounded-pill d-lg-block d-none`}
+                className={`nav-link py-2 my-2 ${location.pathname === "/login" ? "active" : ""} bg-white rounded-pill d-lg-block d-none px-5`}
                 to="/login"
               >
                 Login
               </Link>
               <Link
-                className={`nav-link ${location.pathname === "/login" ? "active" : ""} d-lg-none`}
+                className={`nav-link py-3 ${location.pathname === "/login" ? "active" : ""} d-lg-none`}
                 to="/login"
               >
                 Login
               </Link>
             </li>
-            <li className="nav-item ms-2">
+            <li className="nav-item ms-2 mx-2">
               <Link
-                className={`nav-link ${location.pathname === "/registrar" ? "active" : ""} bg-primary rounded-pill d-lg-block d-none`}
+                className={`nav-link py-2 my-2 ${location.pathname === "/registrar" ? "active" : ""} bg-primary rounded-pill d-lg-block d-none px-5`}
                 to="/registrar"
               >
                 Registro
               </Link>
               <Link
-                className={`nav-link ${location.pathname === "/registrar" ? "active" : ""} d-lg-none`}
+                className={`nav-link py-3 ${location.pathname === "/registrar" ? "active" : ""} d-lg-none`}
                 to="/registrar"
               >
                 Registro
