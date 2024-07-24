421e19b34d4f1346f73d04be98f6da53af8dd053
Miguel Toyas Pernichi <mitoperni@gmail.com>
2024-07-24 15:20:10 +0100

    Arreglada imagen oboarding prueba

--- FRONTEND-REACT/src/pages/OnBoarding.jsx
+++ FRONTEND-REACT/src/pages/OnBoarding.jsx
@@ -13,7 +13,7 @@ export default function Onboarding() {
 
         <div className="min-height-vh mx-auto max-w-720 d-flex justify-content-center align-items-center flex-column m-0 p-0">
             <div className="mb-5">
-                <img src="..\assets\images\photo-1450778869180-41d0601e046e.avif" alt="OnBoardingImage" className="img-fluid" />
+                <img src="https://cdn.pixabay.com/photo/2018/10/01/09/21/pets-3715733_1280.jpg" alt="OnBoardingImage" className="img-fluid" />
             </div>
             <button type="button" className="btn btn-success fs-3" onClick={navigateToLogin}>
                 Ingresar
