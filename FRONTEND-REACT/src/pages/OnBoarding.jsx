eb72968d40cf0cf29a53810cf490c472e86435fe
Miguel Toyas Pernichi <mitoperni@gmail.com>
2024-07-29 15:05:39 +0100

    Cambiando imágenes

--- FRONTEND-REACT/src/pages/OnBoarding.jsx
+++ FRONTEND-REACT/src/pages/OnBoarding.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from 'react-router-dom';
+// import imgOnboarding 
 
 export default function Onboarding() {
 
@@ -13,7 +14,7 @@ export default function Onboarding() {
 
         <div className="min-height-vh mx-auto max-w-720 d-flex justify-content-center align-items-center flex-column m-0 p-0">
             <div className="mb-5">
-                <img src="https://cdn.pixabay.com/photo/2018/10/01/09/21/pets-3715733_1280.jpg" alt="OnBoardingImage" className="img-fluid" />
+                <img src="https://i.imgur.com/dl2LhRL.png" alt="OnBoardingImage" className="img-fluid" />
             </div>
             <button type="button" className="btn btn-success fs-3" onClick={navigateToLogin}>
                 Ingresar
