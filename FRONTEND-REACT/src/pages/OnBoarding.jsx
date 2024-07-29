f2081919bd509c08106fa0e71e4d67551cda3df5
Miguel Toyas Pernichi <mitoperni@gmail.com>
2024-07-29 18:38:53 +0100

    Anuncio cambiado para coger los datos del usuario

--- FRONTEND-REACT/src/pages/OnBoarding.jsx
+++ FRONTEND-REACT/src/pages/OnBoarding.jsx
@@ -14,7 +14,7 @@ export default function Onboarding() {
 
         <div className="min-height-vh mx-auto max-w-720 d-flex justify-content-center align-items-center flex-column m-0 p-0">
             <div className="mb-5">
-                <img src="https://i.imgur.com/dl2LhRL.png" alt="OnBoardingImage" className="img-fluid" />
+                <img src="https://i.imgur.com/PjWTrbb.png" alt="OnBoardingImage" className="img-fluid" />
             </div>
             <button type="button" className="btn btn-success fs-3" onClick={navigateToLogin}>
                 Ingresar
